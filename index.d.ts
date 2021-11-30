f17ecfc961c6a5f7db2c563268449268b6ed70f8
Black-Hole1 <158blackhole@gmail.com>
2021-11-30 12:21:20 +0800

    feat(extension): add apollo-developer-tools extension

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,4 @@ export declare const EMBER_INSPECTOR = string;
 export declare const VUEJS_DEVTOOLS = string;
 export declare const VUEJS3_DEVTOOLS = string;
 export declare const REDUX_DEVTOOLS = string;
+export declare const APOLLO_DEVELOPER_TOOLS = string;
