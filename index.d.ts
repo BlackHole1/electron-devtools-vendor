5be65101a09d17056a35bf54ef40bbbffc8133fc
Black-Hole <158blackhole@gmail.com>
2021-11-30 12:15:51 +0800

    feat(extension): add vuejs3-devtools extension (#5)

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 export declare const REACT_DEVELOPER_TOOLS = string;
 export declare const EMBER_INSPECTOR = string;
 export declare const VUEJS_DEVTOOLS = string;
+export declare const VUEJS3_DEVTOOLS = string;
--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const extensionPath = name => path.join(__dirname, "extensions", name);
 module.exports.REACT_DEVELOPER_TOOLS = extensionPath("react-developer-tools");
 module.exports.EMBER_INSPECTOR = extensionPath("ember-inspector");
 module.exports.VUEJS_DEVTOOLS = extensionPath("vuejs-devtools");
+module.exports.VUEJS3_DEVTOOLS = extensionPath("vuejs3-devtools");
