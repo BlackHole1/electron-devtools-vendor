782998f62682ee76891d245ac26dfc42ee758158
Black-Hole <158blackhole@gmail.com>
2021-11-30 13:25:09 +0800

    feat(extension): add jquery-debugger extension (#11)

--- index.d.ts
+++ index.d.ts
@@ -7,3 +7,4 @@ export declare const APOLLO_DEVELOPER_TOOLS = string;
 export declare const MOBX_DEVTOOLS = string;
 export declare const CYCLEJS_DEVTOOL = string;
 export declare const ANGULARJS_BATARANG = string;
+export declare const JQUERY_DEBUGGER = string;
