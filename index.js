68f05163d3b0f647c348d768412c49bff17fadfe
Black-Hole1 <158blackhole@gmail.com>
2021-11-30 12:08:36 +0800

    refactor(react): rename react-devtools to react-developer-tools

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const path = require("path");
 
 const extensionPath = name => path.join(__dirname, "extensions", name);
 
-module.exports.REACT_DEVTOOLS = extensionPath("react-devtools");
+module.exports.REACT_DEVELOPER_TOOLS = extensionPath("react-developer-tools");
 module.exports.EMBER_INSPECTOR = extensionPath("ember-inspector");
